92a9ae4158f0cdfc542a582707076ed85713a87b
HAN <3257124435@qq.com>
2024-03-22 17:00:14 +0800

    添加了排行榜组件

--- app/src/main/java/com/plcoding/bluetoothchat/presentation/components/Leaderboard.kt
+++ app/src/main/java/com/plcoding/bluetoothchat/presentation/components/Leaderboard.kt
@@ -1,75 +1,93 @@
 package com.plcoding.bluetoothchat.presentation.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * 排行榜组件
+ */
+
+data class LeaderboardItem(val rank: Int, val username: String, val score: Int)
 
 @Composable
-fun GameLeaderboard() {
-    Column {
-        // 前三名柱状图
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .background(Color.Gray)
-                .padding(16.dp)
-        ) {
-            Text(text = "1st Place", color = Color.White)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .background(Color.Gray)
-                .padding(16.dp)
-        ) {
-            Text(text = "2nd Place", color = Color.White)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .background(Color.Gray)
-                .padding(16.dp)
-        ) {
-            Text(text = "3rd Place", color = Color.White)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // 其他排名横向排列
-        Column {
-            repeat(7) { index ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                        .background(Color.Gray)
-                        .padding(16.dp)
-                ) {
-                    Text(text = "${index + 4}th Place", color = Color.White)
-                }
-
-                Spacer(modifier = Modifier.height(8.dp))
+fun Leaderboard(leaderboardItems: List<LeaderboardItem>) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color= Color(0xFFFFFFFF))
+    ){
+        LazyColumn (
+            modifier = Modifier.fillMaxSize()
+        ){
+            items(leaderboardItems) { item ->
+                LeaderboardItemRow(item)
             }
         }
     }
+
 }
+
+@Composable
+fun LeaderboardItemRow(item: LeaderboardItem) {
+    val textColor = when {
+        //item.rank <= 3 -> Color.Red // 前三名使用红色
+        item.rank==1->Color(0xFFC53E14)
+        item.rank==2->Color(0xFFD6691F)
+        item.rank==3->Color(0xFFDABF39)
+        else -> Color(0xFF3D3D3D)
+    }
+    val backgroundColor=when{
+        item.rank<=3->Color(0x1AF7F7F7)
+        else-> Color(0x1AD3D3D3)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .background(color = backgroundColor),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = item.rank.toString(), style = TextStyle(fontSize = 20.sp, color = textColor))
+        Spacer(modifier = Modifier.width(35.dp))
+        Text(text = item.username, style = TextStyle(fontSize = 20.sp, color = textColor))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = item.score.toString(), style = TextStyle(fontSize = 20.sp, color = textColor))
+    }
+}
+
 @Preview
 @Composable
-fun xxx(){
-    GameLeaderboard()
+fun LeaderboardPreview() {
+    val sampleData = listOf(
+        LeaderboardItem(1, "User1", 100),
+        LeaderboardItem(2, "User2", 90),
+        LeaderboardItem(3, "User3", 80),
+        LeaderboardItem(4, "User4", 70),
+        LeaderboardItem(5, "User5", 60),
+        LeaderboardItem(6, "User5", 60),
+        LeaderboardItem(7, "User5", 60),
+        LeaderboardItem(8, "User5", 60),
+        LeaderboardItem(9, "User5", 60),
+        LeaderboardItem(10, "User5", 60),
+        LeaderboardItem(11, "User5", 60)
+    )
+    MaterialTheme {
+        Leaderboard(leaderboardItems = sampleData)
+    }
 }
