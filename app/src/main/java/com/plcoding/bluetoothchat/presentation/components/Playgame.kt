0bdcb9102fc8a458292259cd44f9e97d0579db57
HAN <3257124435@qq.com>
2024-03-24 12:59:57 +0800

    已修改ui,剩余部分：适配结算页面已经确认开始的功能，主页部分，查看战绩，等等.......

--- app/src/main/java/com/plcoding/bluetoothchat/presentation/components/Playgame.kt
+++ app/src/main/java/com/plcoding/bluetoothchat/presentation/components/Playgame.kt
@@ -2,6 +2,7 @@ package com.plcoding.bluetoothchat.presentation.components
 
 import android.content.res.Configuration
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -16,9 +17,11 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.layout.getDefaultLazyLayoutKey
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,7 +31,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -91,8 +99,8 @@ fun  Playgame (
      * 防御模块血量，攻击模块血量，行走模块血量，核心模块是否被摧毁，防御模块装甲值，武器模块伤害，射速，行走模块速度
      */
     var prevent_blood :Float= 100.0F
-    var arms_blood:String="100%"
-    var walk_blood:String="100%"
+    var arms_blood:Float=100.0F
+    var walk_blood:Float=100.0F
     var isDie :String="0"
     var meHurt:String="0"
     var firingRate:String="0"
@@ -100,8 +108,8 @@ fun  Playgame (
     var workRate2:String="0"
     if (message2!=null&& message2.size>=12) {
         prevent_blood = message2[3].toFloat()//防御模块血量
-        arms_blood=message2[4].toString()//武器模块血量
-        walk_blood=message2[5].toString()//行走模块血量
+        arms_blood=message2[4].toFloat()//武器模块血量
+        walk_blood=message2[5].toFloat()//行走模块血量
         isDie =message2[6].toFloat().toString()//核心模块是否被摧毁
         meHurt=message2[7].toFloat().toString()//我方武器模块伤害
         firingRate=message2[8].toFloat().toString()//射速
@@ -133,6 +141,15 @@ fun  Playgame (
             modifier = Modifier.fillMaxSize(),
         ) {
             Column {
+                Image(
+                    painter = painterResource(id = robot(arms_blood,walk_blood)),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(90.dp, 95.dp)
+                        .offset(y = 50.dp, x = 60.dp),
+                    contentScale = ContentScale.FillBounds,
+                    alignment = Alignment.CenterStart
+                )
                 AndroidView(
                     factory =
                     { context ->
@@ -186,7 +203,7 @@ fun  Playgame (
                         }
                     },
                     modifier = Modifier
-                        .offset(y = 75.dp),
+                        .offset(y = 45.dp),
                 )
 
             }
@@ -195,74 +212,114 @@ fun  Playgame (
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Column(
-                    modifier = Modifier.padding(start = 320.dp)
+                Row(
+                    modifier = Modifier.padding(start = 80.dp)
                 ) {
+                   Column(
+                       modifier = Modifier.padding(start = 20.dp)
+                   ) {
+                       Text(text = "武器模块"+arms_blood+"%",
+                           style = TextStyle(fontSize =11.sp ),
+                           color= Color(0x6AEDEDED),
+                           modifier = Modifier
+                               .offset(y=20.dp,x=50.dp)
+                       )
 
-                    Text(text = "血量"+prevent_blood+"%",
-                        style = TextStyle(fontSize =12.sp ),
-                        color= Color(0x6AEDEDED),
-                        modifier = Modifier
-                            .offset(y=20.dp,x=50.dp)
-                    )
-                    LinearProgressIndicator(
-                        progress = prevent_blood/100,
-                        color= Color(0xCDB10000),// 进度值，范围为0.0到1.0
-                        modifier = Modifier
-                            .size(100.dp, 12.dp)
-                            .offset(y = 20.dp, x = 50.dp) // 填充父布局的宽度
-                    )
-                    Text(text = "技能"+message2,
-                        style = TextStyle(fontSize =5.sp ),
-                        color= Color(0x6AEDEDED),
-                        modifier = Modifier
-                            .offset(y=44.dp,x=70.dp)
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.pozhen),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(70.dp, 75.dp)
-                            .offset(y = 50.dp, x = 70.dp),
-                        contentScale = ContentScale.FillBounds,
-                        alignment = Alignment.CenterStart
-                    )
+                       LinearProgressIndicator(
+                           progress = arms_blood/100,
+                           color= Color(0xCDEB6363),// 进度值，范围为0.0到1.0
+                           modifier = Modifier
+                               .size(100.dp, 12.dp)
+                               .offset(y = 20.dp, x = 50.dp) // 填充父布局的宽度
+                       )
 
-                    Card(
-                        shape = RoundedCornerShape(700.dp),
-                        backgroundColor = Color(0x6AEDEDED),
-                        elevation = 4.dp,
-                        modifier = Modifier
-                            .size(100.dp, 100.dp)
-                            .offset(y = 90.dp, x = 67.dp)
-                            .clickable(
-                                enabled = true,
-                                onClickLabel = "开火键",
-                                onClick = {
-                                    fire_b=true
-                                    Log.d("TAG", "Playgame: ${fire}")
-                                    onSendMessage(
-                                        ATS,
-                                        "1" + who + skill + direction + isgame + you_hurt
-                                    )
-                                },
-                                role = Role.Button,
-                                indication = null,
-                                interactionSource = remember { MutableInteractionSource() }
-                            )
+                   }
+                    Column(
+                        modifier = Modifier.padding(start = 20.dp)
                     ) {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                        Text(text = "行走模块"+walk_blood+"%",
+                            style = TextStyle(fontSize =11.sp ),
+                            color= Color(0x6AEDEDED),
+                            modifier = Modifier
+                                .offset(y=20.dp,x=50.dp)
+                        )
+                        LinearProgressIndicator(
+                            progress = walk_blood/100,
+                            color= Color(0xCDFF3A3A),// 进度值，范围为0.0到1.0
+                            modifier = Modifier
+                                .size(100.dp, 12.dp)
+                                .offset(y = 20.dp, x = 50.dp) // 填充父布局的宽度
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.padding(start = 20.dp)
+                    ) {
+                        Text(text = "血量"+prevent_blood+"%",
+                            style = TextStyle(fontSize =11.sp ),
+                            color= Color(0x6AEDEDED),
+                            modifier = Modifier
+                                .offset(y=20.dp,x=50.dp)
+                        )
+                        LinearProgressIndicator(
+                            progress = prevent_blood/100,
+                            color= Color(0xCDB10000),// 进度值，范围为0.0到1.0
+                            modifier = Modifier
+                                .size(100.dp, 12.dp)
+                                .offset(y = 20.dp, x = 50.dp) // 填充父布局的宽度
+                        )
+                        Text(text = "技能"+message2,
+                            style = TextStyle(fontSize =10.sp ),
+                            color= Color(0x6AEDEDED),
+                            modifier = Modifier
+                                .offset(y=50.dp,x=80.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.pozhen),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(70.dp, 75.dp)
+                                .offset(y = 50.dp, x = 70.dp),
+                            contentScale = ContentScale.FillBounds,
+                            alignment = Alignment.CenterStart
+                        )
+
+                        Card(
+                            shape = RoundedCornerShape(700.dp),
+                            backgroundColor = Color(0x6AEDEDED),
+                            elevation = 4.dp,
+                            modifier = Modifier
+                                .size(100.dp, 100.dp)
+                                .offset(y = 90.dp, x = 58.dp)
+                                .clickable(
+                                    enabled = true,
+                                    onClickLabel = "开火键",
+                                    onClick = {
+                                        fire_b = true
+                                        Log.d("TAG", "Playgame: ${fire}")
+                                        onSendMessage(
+                                            ATS,
+                                            "1" + who + skill + direction + isgame + you_hurt
+                                        )
+                                    },
+                                    role = Role.Button,
+                                    indication = null,
+                                    interactionSource = remember { MutableInteractionSource() }
+                                )
                         ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.zidan),
-                                contentDescription = null,
-                                modifier = Modifier.size(50.dp),
-                                contentScale = ContentScale.FillBounds
-                            )
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.zidan),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(50.dp),
+                                    contentScale = ContentScale.FillBounds
+                                )
+                            }
                         }
                     }
+
                 }
             }
         }
@@ -270,8 +327,28 @@ fun  Playgame (
     }
     }
 @Composable
-fun MyComposeLayout() {
+fun RoundedLinearProgressIndicator(preventBlood: Float) {
+    val progress = preventBlood / 100
 
+    Canvas(
+        modifier = Modifier
+            .size(100.dp, 12.dp)
+            .offset(y = 20.dp, x = 50.dp)
+    ) {
+        drawRect(
+            color = Color(0xCDC0BEBE),
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width * progress, size.height),
+            style = Stroke(width = 12f)
+        )
+
+        drawRoundRect(
+            color = Color(0xCDB10000),
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width * progress, size.height),
+            cornerRadius = CornerRadius(6.dp.value, 6.dp.value)
+        )
+    }
 }
 /**
  * 任意毫秒延时函数
@@ -297,6 +374,31 @@ fun parseAsciiString(ascii: String): List<Int> {
         }
 
 }
+fun robot(arm:Float,walk:Float): Int {
+    var armResource = if (arm>50.0&&walk>50.0) {
+        R.drawable.robot
+    } else if(arm<=50.0F&&arm!=0.0F&&walk>50.0){
+        R.drawable.robot_arm1
+    } else if(arm<=50.0F&&arm!=0.0F&&walk<=50.0&&walk!=0.0F){
+
+        R.drawable.robot_w_a
+    }else if(arm==0.0F&&walk>50.0){
+        R.drawable.robot_arm2
+    }else if(arm==0.0F&&walk==0.0F){
+        R.drawable.robot_all
+    }else if(arm>50.0&&walk<=50.0&&walk!=0.0F){
+        R.drawable.robot_walk1
+    }else if(arm>50.0&&walk==0.0F){
+        R.drawable.robot_walk2
+    }else{
+        R.drawable.robot
+    }
+
+
+
+    // 根据arm和walk的值选择不同的图片资源
+   return armResource
+}
 fun hexToDecimal(hex: String): Int {
     return try {
         hex.toInt(16)
@@ -316,12 +418,5 @@ fun extractValueAtIndex(values: List<Int>, index: Int): Int? {
 @Preview(name = "Landscape Preview", showBackground = true, uiMode = Configuration.UI_MODE_TYPE_NORMAL or Configuration.UI_MODE_NIGHT_NO, widthDp = 1000, heightDp = 600)
 @Composable
 fun PreviewMyComposeLayout() {
-    MyComposeLayout()
+
 }
-
-
-
-
-
-
-
